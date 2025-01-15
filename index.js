a0695b6925f9148b4c6f13567389ccad060b8bbe
Ceeylla-Favv <bertleypriscy@gmail.com>
2025-01-15 14:41:01 +0100

    routes updated

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const connectDb = require('./db/connectDb');
-const router = require('./routes/causeRoutes');
-const contributionRoutes = require('./routes/contributionRoutes');
-
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./db/connectDb");
+const router = require("./routes/causeRoutes");
+const contributionRoutes = require("./routes/contributionRoutes");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({
-    origin: "*"
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-
-app.use("/", router);
+app.use("/api/v1/", router);
 app.use("/api/v1/contribute", contributionRoutes);
 
 app.get("/", (req, res) => {
-    res.status(200).json({info: "API working perfectly"})
+  res.status(200).json({ info: "API working perfectly" });
 });
 
 const port = 2025;
 
-app.listen(port, async() => {
-    console.log(`server is working at port ${port}`);
-    await connectDb();
-})
+app.listen(port, async () => {
+  console.log(`server is working at port ${port}`);
+  await connectDb();
+});
